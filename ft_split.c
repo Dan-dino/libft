ba6a84e92c8254ff52f837ee8b08084d5379c71c
Dan <77102436+Dan-dino@users.noreply.github.com>
2023-11-15 08:07:26 +0400

    Update ft_split.c

--- ft_split.c
+++ ft_split.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daalhosa <daalhosa@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/14 13:18:53 by daalhosa          #+#    #+#             */
+/*   Updated: 2023/11/15 08:05:47 by daalhosa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 static size_t	wordcount(char const *str, char del)
@@ -11,15 +23,64 @@ static size_t	wordcount(char const *str, char del)
 		return (0);
 	while (str[i] != '\0')
 	{
-		if (str[i] == del && str[i - 1] != del
-			|| str[i] != del && str[i + 1] == '\0')
+		if (str[i] == del && str[i - 1] != del || str[i] != del && str[i + 1] == '\0')
 			count++;
 		i++;
 	}
 	return (count);
 }
-
-static void	allocate(char *str)
+static void allocate(char const *str, char del)
 {
+	char 	**s;
+	size_t	i;
+	size_t	j;
+	size_t	k;
+	size_t	start;
+	size_t	end;
+
+	i = wordcount(s, del);
+	j = 0;
+	k = 1;
+	if(!str)
+		return (NULL);
+	while(str && j < i)
+	{
+		while(s[k - 1] == del && s[k] !=del || s[k - 1] != del)
+		{
+			start = k;
+			while(s[k] != del)
+				k++;
+			end = k;
+			k++;
+		}
+		s[j] = malloc(sizeof(char *) * (end - start));
+		k++;
+		j++;
+	}
 	
+}
+
+static void free_everything(/*params*/);
+char	**ft_split(char const *s, char c)
+{
+	char 		**string;
+	size_t		count;
+
+	count = wordcount(s, c);
+	string = malloc(sizeof(char *) * (count + 1));
+	if (!string)
+		return (NULL);
+	return(string);
+
+}
+
+int main () { 
+	char str [] = ",,,,sdsds,,,,sdsd,,,sdsf";
+	char **pepino = ft_split(str, ',');
+	int i;
+	i = 0;
+	while(i < 3){
+		printf("%s", pepino[i]);
+		i++;
+	}
 }
