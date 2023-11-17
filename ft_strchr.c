ef63c9ab73dc7cc4e45f1285625a2f9e56de1c01
Dan <77102436+Dan-dino@users.noreply.github.com>
2023-11-17 19:13:49 +0400

    Update ft_strchr.c

--- ft_strchr.c
+++ ft_strchr.c
@@ -17,11 +17,9 @@ char	*ft_strchr(const char *s, int c)
 	size_t	i;
 
 	i = 0;
-	while (s[i] != (char) c)
-	{
-		if (s[i] == '\0')
-			return (NULL);
+	while (s[i] != '\0' && s[i] != (char) c)
 		i++;
-	}
-	return ((char *) s + i);
+	if (s[i] == (char)c)
+		return ((char *) s + i);
+	return (NULL);
 }
